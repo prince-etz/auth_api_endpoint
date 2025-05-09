feb8068c75095c121fd1981f2e7e6e8238307d4d
Prince Umunna <princevictor12@gmail.com>
2025-05-09 12:12:44 +0100

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates :name, :email, :password, presence: true
+  validates :name, :email, :password, presence: true, on: :create
   validates :email, uniqueness: true
+  has_secure_password
 end
